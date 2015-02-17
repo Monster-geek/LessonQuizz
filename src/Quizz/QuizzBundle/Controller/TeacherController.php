b5d7ee98c1fec1fff8fc62fa1acba8d20551decc
Alexandre DIDIER <alexlegrem@gmail.com>
2015-02-17 11:39:27 +0100

    Add classroom.
    Teachers can now create Classrooms.
    
    Controllers :
    - add function to build and process the form.
    
    Routing:
    - add route to the page "Add Classroom"
    
    Template :
    - add the route in the navbar.
    -add the template AddClassroom.

--- src/Quizz/QuizzBundle/Controller/TeacherController.php
+++ src/Quizz/QuizzBundle/Controller/TeacherController.php
@@ -2,8 +2,9 @@
 
 namespace Quizz\QuizzBundle\Controller;
 
-
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Quizz\QuizzBundle\Entity\Classroom;
+use Symfony\Component\HttpFoundation\Request;
 
 class TeacherController extends Controller {
 
@@ -23,6 +24,37 @@ class TeacherController extends Controller {
 
         return $this->render('QuizzQuizzBundle:Teacher:AddStudent.html.twig', array('user'=>$current_user));
 
+    }
+
+    public function addClassroomAction(Request $request)
+    {
+
+        $classroom = new Classroom();
+
+        $classroom->setName('');
+
+        $form = $this->createFormBuilder($classroom)
+            ->setAction($this->generateUrl('teacher_addclassroom'))
+            ->setMethod('POST')
+            ->add('name', 'text', array('attr' => array('class' => 'form-control' , 'placeholder' => 'Libéllé de la classe')))
+            ->add('Ajout' , 'submit' , array('attr'=>array('class'=> 'btn btn-default')))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($classroom);
+            $em->flush();
+
+
+            return $this->redirect($this->generateUrl('teacher_addstudent'));
+        }
+
+        $current_user = $this->get('security.context')->getToken()->getUser();
+
+        return $this->render('QuizzQuizzBundle:Teacher:AddClassroom.html.twig',array('user'=>$current_user , 'form' => $form->createView()));
     }
 
 } 
