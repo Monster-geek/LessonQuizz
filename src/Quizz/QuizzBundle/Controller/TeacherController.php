2825804af9cdd136ec5549858746d86b3a6489d9
Alexandre DIDIER <alexlegrem@gmail.com>
2015-02-18 15:50:01 +0100

    Adding students feature !
    With this commit it's now possible to add students !
    For the moment it's just a P.O.C. (Proof Of Concept). The user is correctly persisted in the database but you can not login with it because the password is randomly generated and not returned in clear value for the moment...

--- src/Quizz/QuizzBundle/Controller/TeacherController.php
+++ src/Quizz/QuizzBundle/Controller/TeacherController.php
@@ -4,6 +4,7 @@ namespace Quizz\QuizzBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Quizz\QuizzBundle\Entity\Classroom;
+use Quizz\QuizzBundle\Entity\Users;
 use Symfony\Component\HttpFoundation\Request;
 
 class TeacherController extends Controller {
@@ -17,12 +18,59 @@ class TeacherController extends Controller {
 
     }
 
-    public function addStudentAction()
+    public function addStudentAction(Request $request)
     {
+        // Init a new user object
+        $student = new Users();
+
+        // Set his role
+        $student->setRoles('ROLE_STUDENT');
+
+        // Building the form
+        $form = $this->createFormBuilder($student)
+            ->setAction($this->generateUrl('teacher_addstudent'))
+            ->setMethod('POST')
+            ->add('name', 'text', array('attr' => array('class' => 'form-control' , 'placeholder' => 'Nom de l\'étudiant')))
+            ->add('lastname' , 'text' ,array('attr' => array('class' => 'form-control' , 'placeholder' => 'Prenom de l\'étudiant')))
+            ->add('email' , 'text' , array('attr' => array('class' => 'form-control' , 'placeholder' => 'Adresse email')))
+            ->add('Ajout' , 'submit' , array('attr'=>array('class'=> 'btn btn-default')))
+            ->getForm();
+
+        //Catch the subit of the form
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+
+            // Time to do some custom operation :)
+
+            // First : let's generate the new password for the student.d
+            $password = uniqid();
+            $factory = $this->get('security.encoder_factory');
+            $encoder = $factory->getEncoder($student);
+            $encodedPassword = $encoder->encodePassword($password, $student->getSalt());
+            $student->setPassword($encodedPassword);
+
+            // Then the username (it must be Unique !)
+            $lastname = $student->getLastname();
+            $lastname = substr(strtolower(rtrim($lastname)), 0 , 1);
+            $username = $lastname.strtolower(rtrim($student->getName()));
+            $student->setUsername($username);
+            // Here doctrine will check if it's ok but will see this later...
+            // TODO : Check the username in the Database !!
+
+
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($student);
+            $em->flush();
+
+
+            return $this->redirect($this->generateUrl('teacher_home'));
+        }
 
         $current_user = $this->get('security.context')->getToken()->getUser();
 
-        return $this->render('QuizzQuizzBundle:Teacher:AddStudent.html.twig', array('user'=>$current_user));
+        return $this->render('QuizzQuizzBundle:Teacher:AddStudent.html.twig', array('user'=>$current_user, 'form' => $form->createView()));
 
     }
 
