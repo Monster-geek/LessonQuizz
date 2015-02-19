d9541304c341bcdc6c0b46b73351638283299a4f
Alexandre DIDIER <alexlegrem@gmail.com>
2015-02-19 01:39:02 +0100

    Improvement of the students adding system.
    After adding a new students his credentials are displayed next to the forms.Will probably deleted in the future and credential will send by mail.

--- src/Quizz/QuizzBundle/Controller/TeacherController.php
+++ src/Quizz/QuizzBundle/Controller/TeacherController.php
@@ -20,6 +20,21 @@ class TeacherController extends Controller {
 
     public function addStudentAction(Request $request)
     {
+
+        // Display the username and the password added
+        // TODO : Implent mail system to send this to the students
+        $add_sucess = $request->query->get('Sucess');
+        if($add_sucess == 'OK')
+        {
+            $allow_box = true;
+            $login_info = array('username'=>$request->query->get('username') , 'password'=>$request->query->get('password'));
+        }
+        else
+        {
+            $allow_box = false;
+            $login_info = array();
+        }
+
         // Init a new user object
         $student = new Users();
 
@@ -43,37 +58,39 @@ class TeacherController extends Controller {
 
             // Time to do some custom operation :)
 
-            // First : let's generate the new password for the student.d
+            // First : let's generate the new password for the student.
+            // Warn : It's not a safe password. It must be change a the first login.
             $password = uniqid();
             $factory = $this->get('security.encoder_factory');
             $encoder = $factory->getEncoder($student);
             $encodedPassword = $encoder->encodePassword($password, $student->getSalt());
             $student->setPassword($encodedPassword);
 
-            // Then the username (it must be Unique !)
+            // Then the username
             $lastname = $student->getLastname();
             $lastname = substr(strtolower(rtrim($lastname)), 0 , 1);
             $username = $lastname.strtolower(rtrim($student->getName()));
 
-            // Here doctrine will check if it's ok but will see this later...
+            // Here doctrine will check if it's ok
             $db_student = $this->getDoctrine()->getRepository('QuizzQuizzBundle:Users');
             $check = $db_student->findBy(array('username'=>$username));
 
-            //
+            // This huge block will check in the database if the username is available.
+            // if not it will add a digit at the end of the username like this : 'example1'
             if(!$check)
             {
+                // If the username is free, let's go !
                 $student->setUsername($username);
                 // Doctrine section. Will persist the user in the database.
                 $em = $this->getDoctrine()->getManager();
                 $em->persist($student);
                 $em->flush();
-
             }
             else
             {
+                // If not let's generate a good one.
                 while($check)
                 {
-                    $check = $db_student->findBy(array('username'=>$username));
                     if($check)
                     {
                         $username = $check[0]->getUsername();
@@ -83,24 +100,21 @@ class TeacherController extends Controller {
                     $username .= $matches[1]+1;
                     $check = $db_student->findBy(array('username'=>$username));
                 }
-
-
                 $student->setUsername($username);
                 // Doctrine section. Will persist the user in the database.
                 $em = $this->getDoctrine()->getManager();
                 $em->persist($student);
                 $em->flush();
-
             }
 
             // Will redirect on the sucess page and display some usefull params like the passwords and the final username.
-            $param = array('password'=>$password , 'username' => $username , 'db_student' => $db_student);
-            return $this->redirect($this->generateUrl('teacher_home',$param));
+            $param = array('password'=>$password , 'username' => $username , 'Sucess' => 'OK');
+            return $this->redirect($this->generateUrl('teacher_addstudent',$param));
         }
 
         $current_user = $this->get('security.context')->getToken()->getUser();
 
-        return $this->render('QuizzQuizzBundle:Teacher:AddStudent.html.twig', array('user'=>$current_user, 'form' => $form->createView()));
+        return $this->render('QuizzQuizzBundle:Teacher:AddStudent.html.twig', array('user'=>$current_user, 'form' => $form->createView() , 'add_box' =>$allow_box , 'login_info' => $login_info));
 
     }
 
